297a41d22daf87f433a9ce3e3157ca41a1a62d5e
Joe Porpeglia <josephp@spotify.com>
2021-08-26 18:35:44 -0400

    Use permission-common in catalog-backend
    Signed-off-by: Joe Porpeglia <josephp@spotify.com>
    Co-authored-by: Tim Hansen <timbonicus@gmail.com>

--- plugins/catalog-backend/src/legacy/service/router.ts
+++ plugins/catalog-backend/src/legacy/service/router.ts
@@ -47,8 +47,10 @@ import {
   requireRequestBody,
   validateRequestBody,
 } from '../../service/util';
-import { AuthorizeResult } from '@backstage/plugin-permission';
-import { PermissionClient } from '@backstage/plugin-permission-module-catalog';
+import {
+  AuthorizeResult,
+  PermissionClient,
+} from '@backstage/permission-common';
 import { IdentityClient } from '@backstage/plugin-auth-backend';
 
 /** @deprecated This was part of the legacy catalog engine */
