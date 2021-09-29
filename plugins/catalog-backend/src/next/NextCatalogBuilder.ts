4bb9091bb73b31ec775dae5130a92e1adb09db5f
nissayeva <nissayeva@users.noreply.github.com>
2021-09-29 17:37:44 -0400

    Use authorization for scaffolder page
    Signed-off-by: nissayeva <nissayeva@users.noreply.github.com>

--- plugins/catalog-backend/src/next/NextCatalogBuilder.ts
+++ plugins/catalog-backend/src/next/NextCatalogBuilder.ts
@@ -295,7 +295,7 @@ export class NextCatalogBuilder {
     locationService: LocationService;
     router: Router;
   }> {
-    const { config, database, logger } = this.env;
+    const { config, database, logger, permissions } = this.env;
 
     const policy = this.buildEntityPolicy();
     const processors = this.buildProcessors();
@@ -326,7 +326,7 @@ export class NextCatalogBuilder {
       parser,
       policy,
     });
-    const entitiesCatalog = new NextEntitiesCatalog(dbClient);
+    const entitiesCatalog = new NextEntitiesCatalog(dbClient, permissions);
     const stitcher = new Stitcher(dbClient, logger);
 
     const locationStore = new DefaultLocationStore(dbClient);
